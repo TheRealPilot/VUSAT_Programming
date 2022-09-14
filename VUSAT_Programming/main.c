35533b7c3201b5f8634f63347576aff70290556a
saltytrain2 <71474292+saltytrain2@users.noreply.github.com>
2022-09-13 21:24:08 -0500

    first commit

---
+++ VUSAT_Programming/main.c
@@ -0,0 +1,18 @@
+/*
+ * VUSAT_Programming.c
+ *
+ * Created: 9/13/2022 21:09:35
+ * Author : salty
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
