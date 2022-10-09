7989267820b6774b39f1c4ccba9e0280bc16ae3b
saltytrain2 <71474292+saltytrain2@users.noreply.github.com>
2022-10-09 15:12:23 -0500

    changed logic to write to csv

--- cameraRun.py
+++ cameraRun.py
@@ -1,158 +1,137 @@
+from collections import defaultdict
 from picamera import PiCamera
-from time import sleep
 import time
 from smbus2 import SMBus
 import FaBo9Axis_MPU9250
 import sys
+import csv
+import os
 
-class humidity:
-    def __init__(self):
-        self.humidity = 0
-        self.temperature = 0
+"""CSV Specification
+humidity: relative humidity measured as a percentage
+humidity_temp: temperature as measured by the humidity sensor in C
+pressure: current air pressure in kPa
+altitude: current height in m
+altimeter_temp: temperature as measured by the altimeter in C
+accel_x: x-direction acceleration in m/s^2
+accel_y: y-direction acceleration in m/s^2
+accel_z: z-direction acceleration in m/s^2
+gyro_x: x-direction angular vel in rad/s
+gyro_y: y-direction angular vel in rad/s
+gryo_z: z-direction angular vel in rad/s
+mag_x: x-direction magnetic field in uT
+mag_y: y-direction magnetic field in uT
+mag_z: z-direction magnetic field in uT
+"""
+HEADERS = ["time", "humidity", "humidity_temp", "pressure", "altitude", "altimeter_temp", "accel_x", "accel_y", "accel_z", "gyro_x", "gyro_y", "gyro_z", "mag_x", "mag_y", "mag_z"]
 
-    def run2(self, time):
-        with SMBus(1) as bus:
+def run_humidity(data_dict:dict):
+    with SMBus(1) as bus:
 
-            # SHT31 address, 0x44(68)
-            bus.write_i2c_block_data(0x44, 0x2C, [0x06])
+        # SHT31 address, 0x44(68)
+        bus.write_i2c_block_data(0x44, 0x2C, [0x06])
 
-            time.sleep(0.5)
+        time.sleep(0.5)
 
-            # SHT31 address, 0x44(68)
-            # Read data back from 0x00(00), 6 bytes
-            # Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
-            data = bus.read_i2c_block_data(0x44, 0x00, 6)
+        # SHT31 address, 0x44(68)
+        # Read data back from 0x00(00), 6 bytes
+        # Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
+        data = bus.read_i2c_block_data(0x44, 0x00, 6)
 
-            # Convert the data
-            temp = data[0] * 256 + data[1]
-            cTemp = -45 + (175 * temp / 65535.0)
-            # fTemp = -49 + (315 * temp / 65535.0)
-            humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+        # Convert the data
+        temp = data[0] * 256 + data[1]
+        cTemp = -45 + (175 * temp / 65535.0)
+        # fTemp = -49 + (315 * temp / 65535.0)
+        humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
 
-            # Output data to screen
-            with open('humidity.txt', 'a') as humidity_file:
-                humidity_file.write(f"Relative Humidity: {humidity:.2f} %RH\n")
+        data_dict['humidity'] = f"{humidity:.2f}"
+        data_dict['humidity_temp'] = f"{cTemp:.2f}"
 
-            with open('temperature1.txt', 'a') as temperature1_file:
-                temperature1_file.write(f'Temperature in Celsius: {cTemp:.2f} C\n')
+def run_altimeter(data_dict:dict):
+    with SMBus(1) as bus:
+        time.sleep(0.25)
+        # MPL3115A2 address, 0x60(96)
+        # Select control register, 0x26(38)
+        #		0xB9(185)	Active mode, OSR = 128, Altimeter mode
+        bus.write_byte_data(0x60, 0x26, 0xB9,True)
+        # MPL3115A2 address, 0x60(96)
+        # Select data configuration register, 0x13(19)
+        #		0x07(07)	Data ready event enabled for altitude, pressure, temperature
+        bus.write_byte_data(0x60, 0x13, 0x07,True)
+        # MPL3115A2 address, 0x60(96)
+        # Select control register, 0x26(38)
+        #		0xB9(185)	Active mode, OSR = 128, Altimeter mode
+        bus.write_byte_data(0x60, 0x26, 0xB9,True)
 
-class Altimeter:
-    def __init__(self):
-        self.pressure = 0
-        self.altitude = 0
-        self.temperature = 0
+        time.sleep(1)
 
-    def run(self, time):
+        # MPL3115A2 address, 0x60(96)
+        # Read data back from 0x00(00), 6 bytes
+        # status, tHeight MSB1, tHeight MSB, tHeight LSB, temp MSB, temp LSB
+        data = bus.read_i2c_block_data(0x60, 0x00, 6)
 
-        # Get I2C bus
-        with SMBus(1) as bus:
-            time.sleep(0.25)
-            # MPL3115A2 address, 0x60(96)
-            # Select control register, 0x26(38)
-            #		0xB9(185)	Active mode, OSR = 128, Altimeter mode
-            bus.write_byte_data(0x60, 0x26, 0xB9,True)
-            # MPL3115A2 address, 0x60(96)
-            # Select data configuration register, 0x13(19)
-            #		0x07(07)	Data ready event enabled for altitude, pressure, temperature
-            bus.write_byte_data(0x60, 0x13, 0x07,True)
-            # MPL3115A2 address, 0x60(96)
-            # Select control register, 0x26(38)
-            #		0xB9(185)	Active mode, OSR = 128, Altimeter mode
-            bus.write_byte_data(0x60, 0x26, 0xB9,True)
+        # Convert the data to 20-bits
+        tHeight = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
+        temp = ((data[4] * 256) + (data[5] & 0xF0)) / 16
+        altitude = tHeight / 16.0
+        cTemp = temp / 16.0
+        # MPL3115A2 address, 0x60(96)
+        # Select control register, 0x26(38)
+        #		0x39(57)	Active mode, OSR = 128, Barometer mode
+        bus.write_byte_data(0x60, 0x26, 0x39)
 
-            time.sleep(1)
+        time.sleep(1)
 
-            # MPL3115A2 address, 0x60(96)
-            # Read data back from 0x00(00), 6 bytes
-            # status, tHeight MSB1, tHeight MSB, tHeight LSB, temp MSB, temp LSB
-            data = bus.read_i2c_block_data(0x60, 0x00, 6)
+        # MPL3115A2 address, 0x60(96)
+        # Read data back from 0x00(00), 4 bytes
+        # status, pres MSB1, pres MSB, pres LSB
+        data = bus.read_i2c_block_data(0x60, 0x00, 4)
 
-            # Convert the data to 20-bits
-            tHeight = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
-            temp = ((data[4] * 256) + (data[5] & 0xF0)) / 16
-            altitude = tHeight / 16.0
-            cTemp = temp / 16.0
-            fTemp = cTemp * 1.8 + 32
+        # Convert the data to 20-bits
+        pres = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
+        pressure = (pres / 4.0) / 1000.0
 
-            # MPL3115A2 address, 0x60(96)
-            # Select control register, 0x26(38)
-            #		0x39(57)	Active mode, OSR = 128, Barometer mode
-            bus.write_byte_data(0x60, 0x26, 0x39)
+        # Output data to file
+        data_dict['pressure'] = f'{pressure:.2f}'
+        data_dict['altitude'] = f'{altitude:.2f}'
+        data_dict['altimeter_temp'] = f'{cTemp:.2f}'
 
-            time.sleep(1)
-
-            # MPL3115A2 address, 0x60(96)
-            # Read data back from 0x00(00), 4 bytes
-            # status, pres MSB1, pres MSB, pres LSB
-            data = bus.read_i2c_block_data(0x60, 0x00, 4)
-
-            # Convert the data to 20-bits
-            pres = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
-            pressure = (pres / 4.0) / 1000.0
-
-            # Output data to file
-            with open('pressure.txt', 'a') as pressure_file:
-                pressure_file.write(f"Pressure: {pressure:.2f} kPa\n")
-
-            with open('altitude.txt', 'a') as altitude_file:
-                altitude_file.write(f'Altitude: {altitude:.2f} m\n')
-
-            with open('temperature.txt', 'a') as temperature_file:
-                temperature_file.write(f'Temperature in Celsius: {cTemp:.2f} C\n')
-
-
-    # def getAlt(self):
-    #     return self.altitude
-    #
-    # def getPres(self):
-    #     return self.pressure
-    #
-    # def getTemp(self):
-    #     return self.temperature
-
-def imu():
+def run_imu(data_dict:dict):
     mpu9250 = FaBo9Axis_MPU9250.MPU9250()
 
     accel = mpu9250.readAccel()
-    with open('acceleration.txt', 'a') as acceleration_file:
-        acceleration_file.write(f"Ax: {accel['x']} m/s^2\n")
-    with open('acceleration.txt', 'a') as acceleration_file:
-        acceleration_file.write(f"Ay: {accel['y']} m/s^2\n")
-    with open('acceleration.txt', 'a') as acceleration_file:
-        acceleration_file.write(f"Az: {accel['z']} m/s^2\n\n")
+    data_dict['accel_x'] = str(accel['x'])
+    data_dict['accel_y'] = str(accel['y'])
+    data_dict['accel_z'] = str(accel['z'])
 
     gyro = mpu9250.readGyro()
-    with open('gyroscope.txt', 'a') as gyroscope_file:
-        gyroscope_file.write(f"Ax: {gyro['x']} rad/s\n")
-    with open('gyroscope.txt', 'a') as gyroscope_file:
-        gyroscope_file.write(f"Ay: {gyro['y']} rad/s\n")
-    with open('gyroscope.txt', 'a') as gyroscope_file:
-        gyroscope_file.write(f"Az: {gyro['z']} rad/s\n")
+    data_dict['gyro_x'] = str(gyro['x'])
+    data_dict['gyro_y'] = str(gyro['y'])
+    data_dict['gyro_z'] = str(gyro['z'])
 
     mag = mpu9250.readMagnet()
-    with open('magnet.txt', 'a') as magnet_file:
-        magnet_file.write(f"Ax: {mag['x']} uT\n")
-        magnet_file.write(f"Ay: {mag['y']} uT\n")
-    with open('magnet.txt', 'a') as magnet_file:
-        magnet_file.write(f"Ay: {mag['y']} uT\n")
-    with open('magnet.txt', 'a') as magnet_file:
-        magnet_file.write(f"Az: {mag['z']} uT\n")
+    data_dict['mag_x'] = str(mag['x'])
+    data_dict['mag_y'] = str(mag['y'])
+    data_dict['mag_z'] = str(mag['z'])
 
 def main() -> int:
     duration = int(sys.argv[1])
-    for i in range(duration):
-        alt = Altimeter()
-        hum = humidity()
-        time.sleep(1)
-        alt.run(time)
-        hum.run2(time)
-        imu()
-        with PiCamera() as camera:
-            camera.resolution=(1920, 1080)
-            camera.start_preview()
-            camera.capture(f'/home/pi/pictures/picture{i}.jpeg', format='jpeg')
-            camera.stop_preview()
-        time.sleep(6.5)       # sets capture interval to 1 minute
+    with open('balloon_stats.csv', 'w', newline='') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=HEADERS)
+        csv_writer.writeheader()
+        for i in range(duration):
+            data = defaultdict(str)
+            data['time']  = time.strftime("%H:%M:%S")
+            run_altimeter(data)
+            run_humidity(data)
+            run_imu(data)
+            csv_writer.writerow(data)
+            with PiCamera() as camera:
+                camera.resolution=(1920, 1080)
+                camera.start_preview()
+                camera.capture(f'/home/pi/pictures/picture{i}.jpeg', format='jpeg')
+                camera.stop_preview()
+            time.sleep(6.5)       # sets capture interval to 1 minute
     return 0
 
 if __name__ == '__main__':
